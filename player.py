6713f6be9bc4fc06e61fee1b363fccde7c7360ba
chxrle <138766818+chxrle@users.noreply.github.com>
2023-07-31 16:27:49 +0100

    Update player.py

--- player.py
+++ player.py
@@ -1,14 +1,14 @@
-class Player:
-    def __init__(self, x, y):
+class Player: #these are the settings and variables deicated to the player character that can be easily customised to whatever the player may desire. 
+    def __init__(self, x, y):  
         self.x = int(x)
         self.y = int(y)
         self.player_size = 10
         self.rect = pygame.Rect(self.x, self.y, self.player_size, self.player_size)
-        self.color = (250, 120, 60)
-        self.velX = 0
+        self.color = (250, 120, 60) 
+        self.velX = 0 
         self.velY = 0
-        self.left_pressed = False
-        self.right_pressed = False
+        self.left_pressed = False     
+        self.right_pressed = False 
         self.up_pressed = False
         self.down_pressed = False
         self.speed = 4
@@ -18,7 +18,7 @@ class Player:
             if cell.x == x and cell.y == y:
                 return cell
 
-    # stops player to pass through walls
+    # stops player to pass through walls 
     def check_move(self, tile, grid_cells, thickness):
         current_cell_x, current_cell_y = self.x // tile, self.y // tile
         current_cell = self.get_current_cell(current_cell_x, current_cell_y, grid_cells)
